cd65d37c09f7f6557335426682ac531712538626
Yuming Zhang <Yuming.Zhang@unige.ch>
2022-01-13 16:02:10 -0500

    add data documentation

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' @title BMI055 Gyroscope Data
+#' @description Six static data sequences collected at 200 Hz for the Bosch BMI055 gyroscope.
+#' @format A \code{matrix} with 4194305 rows and 6 columns corresponding to each of the collected sequence.
+
+"BMI055"
+
+#' @title ICM20689 Gyroscope Data
+#' @description Six static data sequences collected at 200 Hz for the InvenSense ICM-20689 gyroscope.
+#' @format A \code{matrix} with 4194305 rows and 6 columns corresponding to each of the collected sequence.
+
+"ICM20689"
